4957e96e4193025b4c696a514bd42e56b63c622f
Alex Sherman <asherman.ca@gmail.com>
2023-03-21 12:33:35 -0700

    error and loading component

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Loading() {
+	console.log('loading')
+	return <div>Loading</div>
+}
+
+export default Loading
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import MessageList from './MessageList'
 import ChatInput from './ChatInput'
+import { Message } from '../typings'
 
-function HomePage() {
+async function HomePage() {
+	const data = await fetch(
+		`${process.env.VERCEL_URL || 'http://localhost:3000'}/api/getMessages`
+	).then((res) => res.json())
+
+	const messages: Message[] = data.messages
+
 	return (
 		<main>
-			<MessageList />
+			<MessageList initialMessages={messages} />
 			<ChatInput />
-			{/* message list */}
-			{/* chat input */}
 		</main>
 	)
 }
