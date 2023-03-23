179b1017c3377b8512f0ae5094a52a56055d2bba
Alex Sherman <asherman.ca@gmail.com>
2023-03-23 10:47:11 -0700

    middleware auth redirect

--- app/ChatInput.tsx
+++ app/ChatInput.tsx
@@ -47,8 +47,6 @@ function ChatInput({ session }: any) {
 		})
 	}
 
-	console.log('sesh', session)
-
 	return (
 		<form
 			className='fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-t border-gray-100 bg-white'
